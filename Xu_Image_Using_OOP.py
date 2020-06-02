cea970d70488af8d822ac47bc866ab16779cf72e
Nguyễn Đức Linh <50868901+linh22031998@users.noreply.github.com>
2020-06-02 07:39:14 +0700

    new
    add video

--- Xu_Image_Using_OOP.py
+++ Xu_Image_Using_OOP.py
@@ -6,21 +6,17 @@ import imutils
 
 class yolov3(object):
 
-    def __init__(self, weights, config, classes_path, frame, height, width):
+    def __init__(self, weights, config, classes_path):
         self.weights = weights
         self.config = config
         self.classes_path = classes_path
-        self.frame = frame
-        self.height = height
-        self.width = width
         self.confidences = []
         self.class_ids = []
         self.boxes = []
-        self.classes = []
-        self.colors = []
-        self.scale = 0.00392
-        self.input_shape = 608
-        self.processing_image()
+        self.scale = 0.00432 #0.00392
+        self.input_shape = 416
+        self.score_threshold = 0.4
+        self.nms_threshold = 0.4
         self.load_classes()
 
 
@@ -29,18 +25,18 @@ class yolov3(object):
             self.classes = [line.strip() for line in f.readlines()]
         self.colors = np.random.uniform(0, 255, size=(len(self.classes), 3))
 
-    def processing_image(self):
-        print("[INFO] Processing image....")
 
+    def processing_image(self, frame):
+        print("[INFO] Processing....")
+
+        height, width = frame.shape[:2]
         net = cv2.dnn.readNet(self.weights, self.config)
         layer_names = net.getLayerNames()
         outputLayers = [layer_names[i[0]-1] for i in net.getUnconnectedOutLayers()]
 
-        blod = cv2.dnn.blobFromImage(self.frame, self.scale, (self.input_shape, self.input_shape), (0, 0, 0), True, crop=False)
+        blod = cv2.dnn.blobFromImage(frame, self.scale, (self.input_shape, self.input_shape), (0, 0, 0), True, crop=False)
         net.setInput(blod)
-        # print(net)
         outs = net.forward(outputLayers)
-        # print(outs)
 
         class_ids = self.class_ids
         confidences = self.confidences
@@ -52,11 +48,11 @@ class yolov3(object):
                 class_id = np.argmax(scores)
                 # print(class_id)
                 confidence = scores[class_id]
-                if confidence > 0.5:
-                    center_x = int(detection[0]*self.width)
-                    center_y = int(detection[1]*self.height)
-                    w = int(detection[2]*self.width)
-                    h = int(detection[3]*self.height)
+                if confidence > 0.2:
+                    center_x = int(detection[0]*width)
+                    center_y = int(detection[1]*height)
+                    w = int(detection[2]*width)
+                    h = int(detection[3]*height)
 
                     x = int(center_x - w/2)
                     y = int(center_y - h/2)
@@ -64,37 +60,55 @@ class yolov3(object):
                     boxes.append([x, y, w, h])
                     confidences.append(float(confidence))
                     class_ids.append(class_id)
-        self.indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.3, 0.4)
+        self.indexes = cv2.dnn.NMSBoxes(self.boxes, self.confidences, score_threshold=self.score_threshold, nms_threshold=self.nms_threshold)
 
 
-    def draw_boxes(self):
+    def draw_boxes(self, frame):
+        colors = self.colors
         for i in range(len(self.boxes)):
             if i in self.indexes:
                 x, y, w, h = self.boxes[i]
                 label = self.classes[self.class_ids[i]]
                 confidence = self.confidences[i]
-                color = self.colors[self.class_ids[i]]
-
-                cv2.rectangle(self.frame, (x, y), (x+w, y+h), color, 2)
-                cv2.putText(self.frame, label + " " + str(round(confidence, 2)), (x + 1, y + 1), cv2.FONT_HERSHEY_PLAIN, 1,
+                color = colors[self.class_ids[i]]
+                cv2.rectangle(frame, (x, y), (x+w, y+h), color, 2)
+                cv2.putText(frame, label + " " + str(round(confidence, 2)), (x + 1, y + 1), cv2.FONT_HERSHEY_PLAIN, 1,
                             (255, 255, 255), 2)
-        return self.frame
+        return frame
 
 
 if __name__ == "__main__":
-    weights = "./models/weights/yolov3.weights"
-    config = "./models/configs/yolov3-spp3_608.cfg"
-    classes_path = "./data/labels/classes_full.names"
-    video_path = "./data/videos/test1.mp4"
-    image_path = "data\\images\\test10.jpg"
+    weights = "models\\weights\\yolov3-spp3_95_50.weights"
+    config = "models\\configs\\prune_95_50.cfg"
+    classes_path = "data\\labels\\classes_full.names"
+    # video_path = "data\\videos\\test1.mp4"
+    image_path = "data\\images\\test12.jpg"
+    video_path = 0
 
-    image = cv2.imread(image_path)
+    start = yolov3(weights=weights, config=config, classes_path=classes_path)
 
-    height, width, channels = image.shape
-    check = yolov3(frame=image, weights=weights, config=config, classes_path=classes_path, height=height, width=width)
 
-    frame = check.draw_boxes()
-    # frame = cv2.resize(frame, (480, 720))
-    # cv2.imshow("image", frame)
-    cv2.imwrite("Detected_image.jpg", frame)
-    # cv2.waitKey(0)
+    if video_path != 0:
+        cap = cv2.VideoCapture(video_path)
+        while True:
+            try:
+                ret, frame = cap.read()
+                start.processing_image(frame)
+                frame = start.draw_boxes(frame)
+                frame = cv2.resize(frame, (480, 720))
+                cv2.imshow("Detecting", frame)
+                key = cv2.waitKey(1)
+                if key == ord("q"):
+                    break
+            except:
+                print("[INFO] Can't load video...")
+                break
+
+    else:
+        image = cv2.imread(image_path)
+        start.processing_image(image)
+        image = start.draw_boxes(image)
+        cv2.imwrite("Detected_image.jpg", image)
+        image = cv2.resize(image, (480, 720))
+        cv2.imshow("Detected", image)
+        cv2.waitKey(0)
